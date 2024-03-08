9217132279ed732944ffaed13ece6a5bfd9101d7
Max Carpenter <carpenter.max@gmail.com>
2024-03-08 18:36:15 -0500

    dumbing down queue save for Render deploy

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -34,17 +34,17 @@ export const restoreSession = () => async dispatch => {
     let updatedUser = null;
     if (data.user) {
       updatedUser = {...data.user};
-      const storedQueue = JSON.parse(updatedUser.queue)
-      const storedReverseQueue = JSON.parse(updatedUser.reverseQueue);
-      if (storedQueue && storedReverseQueue &&
-         ((storedQueue.length === 0 || (storedQueue.length > 0 && storedQueue.map(songAndTime => new Date() - new Date(songAndTime[0].dateFetched)).sort().slice(-1)[0] < 86400000)) ||
-         (storedReverseQueue.length === 0 || (storedReverseQueue.length > 0 && storedReverseQueue.map(songAndTime => new Date() - new Date(songAndTime[0].dateFetched)).sort().slice(-1)[0] < 86400000)))) {
-          updatedUser.queue = storedQueue
-          updatedUser.reverseQueue = storedReverseQueue
-      } else {
-          updatedUser.queue = []
-          updatedUser.reverseQueue = []
-      }
+      // const storedQueue = JSON.parse(updatedUser.queue)
+      // const storedReverseQueue = JSON.parse(updatedUser.reverseQueue);
+      // if (storedQueue && storedReverseQueue &&
+      //    ((storedQueue.length === 0 || (storedQueue.length > 0 && storedQueue.map(songAndTime => new Date() - new Date(songAndTime[0].dateFetched)).sort().slice(-1)[0] < 86400000)) ||
+      //    (storedReverseQueue.length === 0 || (storedReverseQueue.length > 0 && storedReverseQueue.map(songAndTime => new Date() - new Date(songAndTime[0].dateFetched)).sort().slice(-1)[0] < 86400000)))) {
+      //     updatedUser.queue = storedQueue
+      //     updatedUser.reverseQueue = storedReverseQueue
+      // } else {
+      updatedUser.queue = []
+      updatedUser.reverseQueue = []
+      // }
     }
     storeCurrentUser(updatedUser);
     dispatch(setCurrentUser(updatedUser));
@@ -60,17 +60,17 @@ export const login = ({ credential, password }) => async dispatch => {
     let updatedUser = null;
     if (data.user) {
       updatedUser = {...data.user};
-      const storedQueue = JSON.parse(updatedUser.queue)
-      const storedReverseQueue = JSON.parse(updatedUser.reverseQueue);
-      if (storedQueue && storedReverseQueue &&
-         ((storedQueue.length === 0 || (storedQueue.length > 0 && storedQueue.map(songAndTime => new Date() - new Date(songAndTime[0].dateFetched)).sort().slice(-1)[0] < 86400000)) ||
-         (storedReverseQueue.length === 0 || (storedReverseQueue.length > 0 && storedReverseQueue.map(songAndTime => new Date() - new Date(songAndTime[0].dateFetched)).sort().slice(-1)[0] < 86400000)))) {
-          updatedUser.queue = storedQueue
-          updatedUser.reverseQueue = storedReverseQueue
-      } else {
-          updatedUser.queue = []
-          updatedUser.reverseQueue = []
-      }
+      // const storedQueue = JSON.parse(updatedUser.queue)
+      // const storedReverseQueue = JSON.parse(updatedUser.reverseQueue);
+      // if (storedQueue && storedReverseQueue &&
+      //    ((storedQueue.length === 0 || (storedQueue.length > 0 && storedQueue.map(songAndTime => new Date() - new Date(songAndTime[0].dateFetched)).sort().slice(-1)[0] < 86400000)) ||
+      //    (storedReverseQueue.length === 0 || (storedReverseQueue.length > 0 && storedReverseQueue.map(songAndTime => new Date() - new Date(songAndTime[0].dateFetched)).sort().slice(-1)[0] < 86400000)))) {
+      //     updatedUser.queue = storedQueue
+      //     updatedUser.reverseQueue = storedReverseQueue
+      // } else {
+      updatedUser.queue = []
+      updatedUser.reverseQueue = []
+      // }
     }
     storeCurrentUser(updatedUser);
     dispatch(setCurrentUser(updatedUser));
